65310af53647b2f209f8294524fce565ca520dd4
willyanfx <willyan.fx@gmail.com>
2021-06-18 20:46:50 -0700

    list functions

--- cmd_todos/lib/cmd_todos.ex
+++ cmd_todos/lib/cmd_todos.ex
@@ -1,5 +1,44 @@
 defmodule CmdTodos do
-  def hello do
-    IO.puts('hello world')
+  def create_todos do
+    # number_of_tasks = IO.gets('Enter the number of todos you want to add: ')
+    # {number_of_tasks, _q} = Integer.parse(number_of_tasks)
+    # number_of_tasks
+    {number_of_tasks, _} =
+      IO.gets('Enter the number of todos you want to add: ') |> Integer.parse()
+
+    for _ <- 1..number_of_tasks do
+      IO.gets('Enter Task: ') |> String.trim()
+    end
+  end
+
+  def temp_todos do
+    ['apple', 'banana', 'peach']
+  end
+
+  def complete_todo(tasks, task) do
+    if contains?(tasks, task) do
+      List.delete(tasks, task)
+    else
+      :not_found_error
+    end
+  end
+
+  def add_new_todo(tasks, task) do
+    List.insert_at(tasks, -1, task)
+  end
+
+  def contains?(tasks, task) do
+    Enum.member?(tasks, task)
+  end
+
+  def keyword_search(tasks, word) do
+    for task <- tasks, String.contains?(task, word) do
+      task
+    end
+  end
+
+  def random_task(tasks) do
+    [task] = Enum.take_random(tasks, 1)
+    task
   end
 end
