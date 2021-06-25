47720a3a40c627dcea2f7467d7df5d898bef8bbf
willyanfx <willyan.fx@gmail.com>
2021-06-24 22:18:43 -0700

    create user with account

--- rocketpay/lib/rocketpay_web/controllers/users_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/users_controller.ex
@@ -6,9 +6,11 @@ defmodule RocketpayWeb.UserController do
   action_fallback RocketpayWeb.FallbackController
 
   def create(conn, params) do
+    # "with" verifica o caso, ele devolve o retorno para quem chamou
+    # phx manda para o fallback controller
     with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
       conn
-      |> put_status(:create)
+      |> put_status(:created)
       |> render("create.json", user: user)
     end
   end
