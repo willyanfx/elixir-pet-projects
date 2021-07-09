6eb51b3006b7c81c07a2a10c4a630b169f93fe7b
willyanfx <willyan.fx@gmail.com>
2021-07-08 20:16:23 -0700

    withdraw

--- rocketpay/test/rocketpay_web/controllers/accounts_controller_test.exs
+++ rocketpay/test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -47,4 +47,40 @@ defmodule RocketpayWeb.AccountsControllerTest do
       assert response == expected_response
     end
   end
+
+  describe "withdraw/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "zez",
+        password: "123456",
+        nickname: "ze",
+        email: "zez@banana.com",
+        age: 27
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = Rocketpay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic YmFuYW5hOm5hbmljYTEyMw==")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when all params are valid, widthdraw it", %{conn: conn, account_id: account_id} do
+      conn
+      |> post(Routes.accounts_path(conn, :deposit, account_id, %{"value" => "50.00"}))
+      |> json_response(:ok)
+
+      params = %{"value" => "1.00"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :withdraw, account_id, params))
+        |> json_response(:ok)
+
+      assert %{
+               "account" => %{"balance" => "49.00", "id" => _id},
+               "message" => "Ballance changed successfully"
+             } = response
+    end
+  end
 end
